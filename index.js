853fb4dcd427ea6ec7cb1f8e30f5349ea925bf72
StevenWuTG <stevenwu91@icloud.com>
2020-11-24 15:47:12 -0500

    100% done

--- index.js
+++ index.js
@@ -1,61 +1,100 @@
-
 // element queries
-const name = document.querySelector(".player h2")
-const nickName = document.querySelector("em")
-const likes = document.querySelector(".likes")
-const heart = document.querySelector(".like-button")
-const photo = document.querySelector("img")
-const goalsUl = document.querySelector("ul")
-const form = document.querySelector("form#new-goal-form")
-
+const name = document.querySelector(".player h2");
+const nickName = document.querySelector("em");
+const likes = document.querySelector(".likes");
+const heart = document.querySelector(".like-button");
+const photo = document.querySelector("img");
+const goalsUl = document.querySelector("ul");
+const form = document.querySelector("form#new-goal-form");
+const player = document.querySelector(".player");
 
 //initial Fetch
-function getData(){
-    return fetch('http://localhost:3000/players/1')
-    .then(response => response.json())
-    .then(data => { loadData(data) }) 
-    // console.log(data)
+function getData() {
+  return fetch("http://localhost:3000/players/1")
+    .then((response) => response.json())
+    .then((data) => {
+      loadData(data);
+    });
+  // console.log(data)
 }
-getData()
+getData();
 
+function renderGoal(element) {
+  const li = document.createElement("li");
+  const p = document.createElement("p");
+  const a = document.createElement("a");
+
+  p.textContent = element.description;
+  a.href = element.link;
+  a.textContent = element.link;
+
+  li.append(a);
+  li.append(p);
+  goalsUl.append(li);
+}
 
 // player load
-function loadData (player) {
-    // debugger
-    nickName.textContent = player.nickname
-    name.textContent = player.name
-    likes.textContent = player.likes
-    photo.src = player.photo
-    const goalsArray = player.goals
-    goalsArray.forEach(element => {
-        const li = document.createElement("li")
-        const p = document.createElement("p")
-        const a = document.createElement("a")
-        
-        p.textContent = element.description
-        a.href = element.link
-        a.textContent = element.link
+function loadData(playerObj) {
+  // debugger
+  player.id = playerObj.id;
 
-        li.append(a)
-        li.append(p)
-        goalsUl.append(li)
-    })
+  nickName.textContent = playerObj.nickname;
+  name.textContent = playerObj.name;
+  likes.textContent = playerObj.likes;
+  photo.src = playerObj.photo;
+  const goalsArray = playerObj.goals;
+  goalsArray.forEach((element) => {
+    renderGoal(element);
+    // const li = document.createElement("li");
+    // const p = document.createElement("p");
+    // const a = document.createElement("a");
+
+    // p.textContent = element.description;
+    // a.href = element.link;
+    // a.textContent = element.link;
+
+    // li.append(a);
+    // li.append(p);
+    // goalsUl.append(li);
+  });
 }
 
+heart.addEventListener("click", function (e) {
+  fetch("http://localhost:3000/players/1", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      likes: `${parseInt(likes.textContent) + 1}`,
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((updatedLikes) => {
+      likes.textContent = `${updatedLikes.likes} Likes`;
+    });
+});
 
-heart.addEventListener("click", function(e){
-    fetch("http://localhost:3000/players/1", {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            likes: `${parseInt(likes.textContent) + 1}`
-        })
-    })
-    .then(resp => resp.json())
-    .then(updatedLikes => {
-        
-        likes.textContent = `${updatedLikes.likes} Likes` 
-    })
-})
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const newGoal = {
+    playerId: parseInt(player.id),
+    link: e.target.link.value,
+    description: e.target.description.value,
+  };
+  debugger;
+
+  fetch("http://localhost:3000/goals", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newGoal),
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+    });
+
+  renderGoal(newGoal);
+});
